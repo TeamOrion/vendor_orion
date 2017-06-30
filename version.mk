c6f599bc0acfc49dfd7d1a1b2affe3c75bc67497
Akhil Narang <akhilnarang.1999@gmail.com>
2017-06-30 11:58:01 +0530

    version: add some props
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- version.mk
+++ version.mk
@@ -5,3 +5,7 @@ CODENAME := Andromeda
 ORION_BUILDTYPE ?= Unofficial
 
 ORION_VERSION := OrionOS-$(VERSION)-$(CODENAME)-$(TARGET_DEVICE)-$(ORION_BUILDTYPE)-$(shell date +%Y%m%d)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.orion.version=$(ORION_VERSION) \
+    ro.orion.buildtype=$(ORION_BUILDTYPE)
