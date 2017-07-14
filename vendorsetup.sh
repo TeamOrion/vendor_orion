4eca2af9c808aee9b8bdc038441cb97d9f186fb6
desaishivam26 <shivamdesaixda@gmail.com>
2017-07-14 18:48:19 +0000

    vendor: export TARGET_DEVICE too

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,8 @@ if [ -z "$variant" ]; then
     variant="userdebug";
 fi
 
-for d in $(cat vendor/orion/orion.devices); do
+for d in $(cat vendor/orion/orion.devices); do {
+    export TARGET_DEVICE=$d;
     add_lunch_combo orion_$d-$variant;
+}
 done
