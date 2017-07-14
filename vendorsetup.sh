8a78d5cdedcd5b95e519faa44ab3f8d4d1fb643d
desaishivam26 <shivamdesaixda@gmail.com>
2017-07-14 17:20:26 +0000

    Revert "vendor: export TARGET_DEVICE too"
    This reverts commit 8f60c9eabd2bcc65810d42fc6298e214af00fede.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,6 @@ if [ -z "$variant" ]; then
     variant="userdebug";
 fi
 
-for d in $(cat vendor/orion/orion.devices); do {
-    export TARGET_DEVICE=$d;
+for d in $(cat vendor/orion/orion.devices); do
     add_lunch_combo orion_$d-$variant;
-}
 done
